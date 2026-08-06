05ccc14c3b191ecb3f5b73ed02f467acb4f5f8a7
Walter Spanu <walter.spanu@horsa.it>
2026-08-06 12:40:18 +0200

    Fix sui valori non mostrati dei creator

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,14 @@ import {
   loadPlayers,
 } from "@/lib/players";
 
+export const revalidate = 60;
+
 export default async function Home() {
   const [players, lastUpdate] = await Promise.all([
     loadPlayers(),
     loadLastUpdate(),
   ]);
+
   const strategyColumns = getStrategyColumns(players);
 
   return (
