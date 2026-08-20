a84fdf670ea36f15387fc040800f40bbb947d1f9
Walter Spanu <walter.spanu@horsa.it>
2026-08-20 10:17:19 +0200

    Add Vercel Analytics and update Next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 
 import ThemeInitializer from "@/components/theme-initializer";
 
@@ -40,6 +41,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <ThemeInitializer />
         {children}
+		<Analytics />
       </body>
     </html>
   );
